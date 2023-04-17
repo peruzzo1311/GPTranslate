9d5d0c5b89d6e9160755a84ab8a8eca277793df7
Gustavo Peruzzo <gustavoperuzzo1311@hotmail.com>
2023-04-17 15:06:35 -0300

    v1 	deleted:    .gitignore 	deleted:    @sinonjs/commons/LICENSE 	deleted:    @sinonjs/commons/README.md 	deleted:    @sinonjs/commons/lib/called-in-order.js 	deleted:    @sinonjs/commons/lib/called-in-order.test.js 	deleted:    @sinonjs/commons/lib/class-name.js 	deleted:    @sinonjs/commons/lib/class-name.test.js 	deleted:    @sinonjs/commons/lib/deprecated.js 	deleted:    @sinonjs/commons/lib/deprecated.test.js 	deleted:    @sinonjs/commons/lib/every.js 	deleted:    @sinonjs/commons/lib/every.test.js 	deleted:    @sinonjs/commons/lib/function-name.js 	deleted:    @sinonjs/commons/lib/function-name.test.js 	deleted:    @sinonjs/commons/lib/global.js 	deleted:    @sinonjs/commons/lib/global.test.js 	deleted:    @sinonjs/commons/lib/index.js 	deleted:    @sinonjs/commons/lib/index.test.js 	deleted:    @sinonjs/commons/lib/order-by-first-call.js 	deleted:    @sinonjs/commons/lib/order-by-first-call.test.js 	deleted:    @sinonjs/commons/lib/prototypes/README.md 	deleted:    @sinonjs/commons/lib/prototypes/array.js 	deleted:    @sinonjs/commons/lib/prototypes/copy-prototype-methods.js 	deleted:    @sinonjs/commons/lib/prototypes/copy-prototype-methods.test.js 	deleted:    @sinonjs/commons/lib/prototypes/function.js 	deleted:    @sinonjs/commons/lib/prototypes/index.js 	deleted:    @sinonjs/commons/lib/prototypes/index.test.js 	deleted:    @sinonjs/commons/lib/prototypes/map.js 	deleted:    @sinonjs/commons/lib/prototypes/object.js 	deleted:    @sinonjs/commons/lib/prototypes/set.js 	deleted:    @sinonjs/commons/lib/prototypes/string.js 	deleted:    @sinonjs/commons/lib/prototypes/throws-on-proto.js 	deleted:    @sinonjs/commons/lib/type-of.js 	deleted:    @sinonjs/commons/lib/type-of.test.js 	deleted:    @sinonjs/commons/lib/value-to-string.js 	deleted:    @sinonjs/commons/lib/value-to-string.test.js 	deleted:    @sinonjs/commons/package.json 	deleted:    @sinonjs/commons/types/called-in-order.d.ts 	deleted:    @sinonjs/commons/types/class-name.d.ts 	deleted:    @sinonjs/commons/types/deprecated.d.ts 	deleted:    @sinonjs/commons/types/every.d.ts 	deleted:    @sinonjs/commons/types/function-name.d.ts 	deleted:    @sinonjs/commons/types/global.d.ts 	deleted:    @sinonjs/commons/types/index.d.ts 	deleted:    @sinonjs/commons/types/order-by-first-call.d.ts 	deleted:    @sinonjs/commons/types/prototypes/array.d.ts 	deleted:    @sinonjs/commons/types/prototypes/copy-prototype-methods.d.ts 	deleted:    @sinonjs/commons/types/prototypes/function.d.ts 	deleted:    @sinonjs/commons/types/prototypes/index.d.ts 	deleted:    @sinonjs/commons/types/prototypes/map.d.ts 	deleted:    @sinonjs/commons/types/prototypes/object.d.ts 	deleted:    @sinonjs/commons/types/prototypes/set.d.ts 	deleted:    @sinonjs/commons/types/prototypes/string.d.ts 	deleted:    @sinonjs/commons/types/prototypes/throws-on-proto.d.ts 	deleted:    @sinonjs/commons/types/type-of.d.ts 	deleted:    @sinonjs/commons/types/value-to-string.d.ts 	deleted:    @sinonjs/fake-timers/LICENSE 	deleted:    @sinonjs/fake-timers/README.md 	deleted:    @sinonjs/fake-timers/package.json 	deleted:    @sinonjs/fake-timers/src/fake-timers-src.js 	deleted:    App.js 	deleted:    app.json 	deleted:    assets/adaptive-icon.png 	deleted:    assets/favicon.png 	deleted:    assets/icon.png 	deleted:    assets/splash.png 	deleted:    babel.config.js 	deleted:    eas.json 	deleted:    package-lock.json 	deleted:    package.json 	deleted:    src/Languages.js 	deleted:    src/components/Header/index.tsx 	deleted:    src/components/Header/styles.tsx 	deleted:    src/pages/Home/index.tsx 	deleted:    src/pages/Home/styles.tsx 	deleted:    src/routes/app.routes.tsx 	deleted:    src/services/index.ts 	deleted:    src/store/context/slice.ts 	deleted:    src/store/hooks.ts 	deleted:    src/store/index.ts 	deleted:    src/store/language/slice.ts 	deleted:    src/store/targetLanguage/slice.ts 	deleted:    tsconfig.json

--- src/store/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux'
-import React from 'react'
-
-import type { TypedUseSelectorHook } from 'react-redux'
-import type { RootState, AppDispatch } from './index'
-
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
